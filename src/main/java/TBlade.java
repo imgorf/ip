5feba8f67994fec9542f394126df31d72fbf7eee
Jason Loh <lohjason808@gmail.com>
2026-08-19 22:07:55 +0800

    Level-0

---
+++ src/main/java/TBlade.java
@@ -0,0 +1,18 @@
+public class TBlade {
+    public static void main(String[] args) {
+        String separator = "_".repeat(60);
+        String banner = "#####  ####   #       ###   ####   #####\n"
+                + "  #    #   #  #      #   #  #   #  #\n"
+                + "  #    ####   #      #####  #   #  ####\n"
+                + "  #    #   #  #      #   #  #   #  #\n"
+                + "  #    ####   #####  #   #  ####   #####";
+
+        System.out.println(separator);
+        System.out.println(banner);
+        System.out.println("Hello! I'm TBlade.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
